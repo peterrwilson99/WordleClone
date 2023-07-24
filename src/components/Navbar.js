5e1d7ee630683659ba411f637334883786eb5912
Peter Wilson <peterrwilson99@gmail.com>
2023-07-24 14:47:29 -0700

    navbar changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import { Button, ButtonBase, Divider, IconButton, Typography, useMediaQuery, useTheme } from '@mui/material'
+import { ButtonBase, Divider, IconButton, Typography, useMediaQuery, useTheme } from '@mui/material'
 import MenuIcon from '@mui/icons-material/Menu';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import SettingsIcon from '@mui/icons-material/Settings';
@@ -18,10 +18,11 @@ function Navbar() {
     }
 
     const isSmallScreen = useMediaQuery(theme.breakpoints.down('sm'));
+    console.log(isSmallScreen)
     
     return (
-        <nav className="min-h-[65px] py-1">
-            <div className="px-4 min-h-[65px] grid grid-cols-3">
+        <nav className="py-1">
+            <div className={`px-4 min-h-[${isSmallScreen ? '50px' : '65px'}] ${isSmallScreen ? 'flex justify-between' : 'grid grid-cols-3'}`}>
                 <div className="my-auto">
                     <IconButton onClick={handleHamburgerClick} >
                         <MenuIcon sx={{
@@ -29,47 +30,62 @@ function Navbar() {
                             height: '24px',
                         }}/>
                     </IconButton>
+                    {
+                        isSmallScreen ?
+                            <Typography variant="h7" className="font-extrabold" align="left" >
+                                Wordle Clone
+                            </Typography>
+                            :
+                            <></>
+                    }
                 </div>
-                <div className="my-auto">
-                    <Typography variant={isSmallScreen ? "h7" : "h5"} className="font-extrabold" align="center" >
-                        Wordle Clone
-                    </Typography>
+                <div className={`my-auto ${isSmallScreen ? 'w-0' : ''}`}>
+                    {
+                        isSmallScreen ?
+                            <></>
+                            :
+                            <Typography variant="h5" className="font-extrabold" align="center" >
+                                Wordle Clone
+                            </Typography>
+                    }
                 </div>
-                <div className="my-auto">
-                    <div className="flex max-w-[300px] flex-row justify-between align-middle items-center">
-                        <div>
-                            <IconButton sx={{paddingX: '5px', display: isSmallScreen ? 'none' : ''}} onClick={handleInfoClick} >
-                                <HelpOutlineIcon sx={{
-                                    color: theme.palette.text.primary,
-                                    height: isSmallScreen ? '20px' : '30px',
-                                    width: isSmallScreen ? '20px' : '30px',
-                                }}/>
-                            </IconButton>
-                            <IconButton sx={{paddingX: '5px', display: isSmallScreen ? 'none' : ''}} onClick={() => window.open('https://github.com/peterrwilson99/', '_blank')} >
-                                <GitHubIcon sx={{
-                                    color: theme.palette.text.primary,
-                                    height: isSmallScreen ? '20px' : '28px',
-                                    width: isSmallScreen ? '20px' : '28px',
-                                }}/>
-                            </IconButton>
-                            <IconButton sx={{paddingX: '5px'}} onClick={handleSettingsClick} >
-                                <SettingsIcon sx={{
-                                    color: theme.palette.text.primary,
-                                    height: isSmallScreen ? '20px' : '30px',
-                                    width: isSmallScreen ? '20px' : '30px',
-                                }}/>
-                            </IconButton>
-                        </div>
-                        <div>
-                            <ButtonBase variant="outlined" size="small" className="text-xs py-1.5 px-6 font-medium" sx={{
-                                borderRadius: '20px',
-                                border: `1px solid ${theme.palette.miss.main}}`,
+                <div className="my-auto flex justify-end">
+                    <div>
+                        <IconButton sx={{paddingX: '5px', display: isSmallScreen}} onClick={handleInfoClick} >
+                            <HelpOutlineIcon sx={{
                                 color: theme.palette.text.primary,
-                                textTransform: 'none',
-                            }} color="inherit" onClick={() => window.open('https://www.nytimes.com/games/wordle/index.html', '_blank')}>
-                                Play Real Wordle
-                            </ButtonBase>
-                        </div>
+                                height: '30px',
+                                width: '30px',
+                            }}/>
+                        </IconButton>
+                        <IconButton sx={{paddingX: '5px', display: isSmallScreen}} onClick={() => window.open('https://github.com/peterrwilson99/', '_blank')} >
+                            <GitHubIcon sx={{
+                                color: theme.palette.text.primary,
+                                height: '28px',
+                                width: '28px',
+                            }}/>
+                        </IconButton>
+                        <IconButton sx={{paddingX: '5px'}} onClick={handleSettingsClick} >
+                            <SettingsIcon sx={{
+                                color: theme.palette.text.primary,
+                                height: '30px',
+                                width: '30px',
+                            }}/>
+                        </IconButton>
+                        {
+                            isSmallScreen ?
+                                <></>
+                                :
+                                <ButtonBase variant="outlined" size="small" className="text-xs py-1.5 px-6 ml-6 font-medium" sx={{
+                                    borderRadius: '20px',
+                                    border: `1px solid ${theme.palette.miss.main}}`,
+                                    color: theme.palette.text.primary,
+                                    textTransform: 'none',
+                                }} color="inherit" onClick={() => window.open('https://www.nytimes.com/games/wordle/index.html', '_blank')}>
+                                    Play Real Wordle
+                                </ButtonBase>
+                        }
+
                     </div>
                 </div>
             </div>
